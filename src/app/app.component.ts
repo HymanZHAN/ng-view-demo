2db19e17a2c24eac5bb28cb647d32a31d541c140
Xucong Zhan <xzhan@connect.ust.hk>
2023-02-15 18:04:48 +0800

    feat: persist tab selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { Component, inject, Inject, OnInit, PLATFORM_ID } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,24 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "view-demo";
+  platformId: Object;
+  storageKey = "selectedTab";
+
+  private _selectedIndex = 0;
+  get selectedIndex() {
+    return this._selectedIndex;
+  }
+  set selectedIndex(value: number) {
+    this._selectedIndex = value;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(this.storageKey, value.toString());
+    }
+  }
+
+  constructor() {
+    this.platformId = inject(PLATFORM_ID);
+    if (isPlatformBrowser(this.platformId)) {
+      this.selectedIndex = Number(localStorage.getItem(this.storageKey));
+    }
+  }
 }
