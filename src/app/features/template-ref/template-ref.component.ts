20683cfc1526fadc655613fd071ff01c9562a36e
Xucong Zhan <xzhan@connect.ust.hk>
2023-03-06 23:56:26 +0800

    feat: first pass TemplateRef

--- src/app/features/template-ref/template-ref.component.ts
+++ src/app/features/template-ref/template-ref.component.ts
@@ -1,9 +1,40 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  Component,
+  EmbeddedViewRef,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
+import {
+  PlusOutlineComponent,
+  RefreshOutlineComponent,
+  TrashOutlineComponent,
+} from "@components/icons";
 
 @Component({
   selector: "app-template-ref",
   standalone: true,
-  template: ` <p>template-ref works!</p> `,
-  styles: [],
+  imports: [CommonModule, PlusOutlineComponent, TrashOutlineComponent, RefreshOutlineComponent],
+  templateUrl: "./template-ref.component.html",
+  styles: [
+    `
+      :host {
+        @apply flex flex-col justify-center items-center w-full;
+      }
+    `,
+  ],
 })
-export default class TemplateRefComponent {}
+export default class TemplateRefComponent {
+  @ViewChild("listItems", { read: ViewContainerRef }) container!: ViewContainerRef;
+  @ViewChild("listItemTemplate", { read: TemplateRef }) listItemTemplate!: TemplateRef<unknown>;
+
+  counter = 0;
+  itemViewRefs: Map<string, EmbeddedViewRef<unknown>> = new Map();
+
+  addItem() {
+    const name = `Item ${++this.counter}`;
+    const vr = this.container?.createEmbeddedView(this.listItemTemplate, { name });
+    this.itemViewRefs.set(name, vr);
+  }
+}
--- src/app/features/view-container-ref/manipulate-embedded-view/manipulate-embedded-view.component.ts
+++ src/app/features/view-container-ref/manipulate-embedded-view/manipulate-embedded-view.component.ts
@@ -27,9 +27,13 @@ import {
     RefreshOutlineComponent,
   ],
   templateUrl: "./manipulate-embedded-view.component.html",
-  styles: [`:host {
-    @apply flex flex-col justify-center items-center w-full;
-  }`],
+  styles: [
+    `
+      :host {
+        @apply flex flex-col justify-center items-center w-full;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ManipulateEmbeddedViewComponent {
